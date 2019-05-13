4f1dbb3c1d67a9f456a0ac97e6e7555780c1a7c5
lixianxun <13023218819@163.com>
2019-05-13 17:14:28 +0800

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -50,3 +50,16 @@ print(OR(0,0))
 print(OR(1,1))
 print(OR(1,0))
 print(OR(0,1))
+
+
+def XOR(x1, x2):
+  s1 = NAND(x1, x2)
+  s2 = OR(x1, x2)
+  y = AND(s1, s2)
+  return y
+
+print("---XOR---")
+print(XOR(0,0))
+print(XOR(1,1))
+print(XOR(1,0))
+print(XOR(0,1))
